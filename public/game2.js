9131df42d0da03d6f44e1c00676aed167b7105d8
tamburrisam <tamburrisam@gmail.com>
2021-07-21 16:53:57 -0400

    added functionality onto noDuplicates function which basically loads a new list if the page is refreshed as to not clog up the users DB

--- public/game2.js
+++ public/game2.js
@@ -36,17 +36,72 @@ auth.onAuthStateChanged((user) => {
   function noDuplicates(list, secondList){
     let inputList = document.querySelector('#word-list')
   let arr = ['sdfsdf','asdfa']
+    let indices = [];
+  
+  
   
     let room = db.collection('users').doc(auth.currentUser.uid)
   
     room.get().then((doc) => {
       console.log(`general list length`, list.length)
       console.log(`my list length`, doc.data().list_one_input.length)
-  
+
+      for(let i = 0; i < doc.data().list_one_input.length; i++){
+        indices.push(i)
+      }
+  console.log(`INDICES`, indices)
   
       console.log(`your list two input from rooms db`,doc.data().list_one_input)
   
       console.log(`random list_two from db`,list)
+
+      list_one_received = list
+
+      if(doc.data().list_one_received){
+        if(doc.data().list_one_received.length > 0){
+          console.log('ok')
+          //set with merge overwrites the field we want
+          //without merge it would override the whole document
+          room.set({
+            list_one_received
+        }, {merge: true})
+
+        let html = ''
+        list.forEach((word) => {
+          html += `<li>${word}</li>`
+        })
+        inputList.innerHTML = html  
+        console.log('good')
+
+        if(arraysEqual(list, doc.data().list_one_input) == true){
+          console.log('trueeeeee')
+
+          noDuplicates(secondList)
+          secondList.forEach((word) => {
+            room.update({
+                list_one_received: firebase.firestore.FieldValue.arrayUnion(word)
+            }).then(() => {
+                console.log('list two received added')
+            }).catch((err) => {
+                console.log(err)
+            }) 
+            console.log('fetched from list_two')
+        }) 
+        }else{
+          list.forEach((word) => {
+            room.update({
+                list_one_received: firebase.firestore.FieldValue.arrayUnion(word)
+            }).then(() => {
+                console.log('list one received added')
+            }).catch((err) => {
+                console.log(err)
+            }) 
+            console.log('fetched from list_two')
+        }) 
+        }
+      }else{
+      //DELETE WHATS IN THE RECEIVED LIST FIRST
+      //THEN ADD
 
   
   
@@ -85,6 +140,8 @@ auth.onAuthStateChanged((user) => {
                 }
   
                 console.log(arraysEqual(list, doc.data().list_one_input))
+              }
+            }
     })
   }
 
